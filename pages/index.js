fb5e06f574d9fca9bb312a4f50ab14de0bac2867
Ruhan Khandakar <52363470+ruhankhandakar@users.noreply.github.com>
2021-06-05 13:37:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Head from 'next/head'
 
 export const getStaticProps = async () => {
-  const url = `https://webcontent-pickrr.herokuapp.com/courier-info`;
-  const response = await fetch(url);
-  const allCarriers = await response.json();
-
   return {
     props: {
-      data: allCarriers.data,
-      revalidate: 1,
       updateAt: Date.now(),
     },
   };
@@ -20,17 +14,6 @@ export default function Home({ data, updateAt }) {
   return (
     <div style={{ backgroundColor: 'teal' }}>
       {timeString}
-      {data.map((carrier) => (
-        <div key={carrier.courier_name}>
-          <div> {carrier.courier_name}</div>
-
-          <button
-            onClick={() => router.push(`/carrier-integrations/${carrier.slug}`)}
-          >
-            Read More
-          </button>
-        </div>
-      ))}
     </div>
   )
 }
