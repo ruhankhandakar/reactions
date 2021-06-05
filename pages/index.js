38fffa7c38a6c8ffc1a3057c35844b66281e85cc
Ruhan Khandakar <52363470+ruhankhandakar@users.noreply.github.com>
2021-06-05 13:29:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 export const getStaticProps = async () => {
-  const url = `https://webcontent-pickrr.herokuapp.com//courier-info/`;
+  const url = `https://webcontent-pickrr.herokuapp.com/courier-info`;
   const response = await fetch(url);
   const allCarriers = await response.json();
 
@@ -18,39 +18,19 @@ export const getStaticProps = async () => {
 export default function Home({ reactions, updatedAt }) {
   const timeString = new Date(updatedAt).toLocaleTimeString();
   return (
-    <div className='container'>
-      <Head>
-        <title>Static Reactions Demo</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta property='og:title' content='Static Reactions Demo' />
-        <meta
-          property='og:description'
-          content='Using Next.js Incremental Static Regeneration'
-        />
-        <meta
-          property='og:image'
-          content='https://og-image.now.sh/Incremental%20Static%20Regeneration%20Demo%20using%20**GitHub%20Reactions**.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
-        />
-      </Head>
+    <div style={{ backgroundColor: 'teal' }}>
+      {updateAt}
+      {data.map((carrier) => (
+        <div key={carrier.courier_name}>
+          <div> {carrier.courier_name}</div>
 
-      <main>
-           <div style={{ backgroundColor: 'teal' }}>
-            {timeString}
-            {data.map((carrier) => (
-              <div key={carrier.courier_name}>
-                <div> {carrier.courier_name}</div>
-
-                <button
-                  onClick={() => router.push(`/carrier-integrations/${carrier.slug}`)}
-                >
-                  Read More
-                </button>
-              </div>
-            ))}
-          </div>
-
-      </main>
+          <button
+            onClick={() => router.push(`/carrier-integrations/${carrier.slug}`)}
+          >
+            Read More
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
